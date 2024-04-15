e24b408fc3e60029874f75799b5240b63755163d
NguyenTriKhang <rokkhangnvd@outlook.com.vn>
2024-04-15 18:19:06 +0700

    fix cors setting

--- server/src/main/java/com/example/ChatApp/Config/CorsConfigure.java
+++ server/src/main/java/com/example/ChatApp/Config/CorsConfigure.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfigure implements WebMvcConfigurer {
 	@Override public void addCorsMappings(CorsRegistry registry) { 
 		  registry.addMapping("/**")
-		//   .allowedOrigins("http://localhost:3000/")
+		  .allowedOrigins("http://localhost:3000/")
 		  .allowedMethods("GET","POST","PUT","DELETE")
-		  .allowedHeaders("*");
-		//   .allowCredentials(true);
+		  .allowedHeaders("*")
+		  .allowCredentials(true);
 		 }
 }
