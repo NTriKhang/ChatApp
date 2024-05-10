6836615ad7a9bb76e9746b7403ed0139ff7d2913
ThanhLuan <ntluan51@gmail.com>
2024-05-10 21:50:15 +0700

    FixDate

--- server/src/main/java/com/example/ChatApp/Models/Submodels/LastMessage_MsgGroup.java
+++ server/src/main/java/com/example/ChatApp/Models/Submodels/LastMessage_MsgGroup.java
@@ -1,12 +1,7 @@
 package com.example.ChatApp.Models.Submodels;
 
 import java.sql.Date;
-<<<<<<< HEAD
-import java.time.LocalDate;
-
-=======
 import java.time.LocalDateTime;
->>>>>>> ce60f64576769daac99414c80bc13539a27b7053
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
@@ -15,15 +10,6 @@ public class LastMessage_MsgGroup {
 	public String message_id;
 	public String content;
 	public String user_name;
-<<<<<<< HEAD
-	public LocalDate created_date;
-	
-	public LastMessage_MsgGroup() {
-		
-	}
-	
-	public LastMessage_MsgGroup(String message_id, String content, String user_name, LocalDate created_date) {
-=======
 	public LocalDateTime created_date;	
   
 	public LastMessage_MsgGroup() {
@@ -35,7 +21,6 @@ public class LastMessage_MsgGroup {
 		created_date = cDateTime;
 	}
 	public LastMessage_MsgGroup(String message_id, String content, String user_name, LocalDateTime created_date) {
->>>>>>> ce60f64576769daac99414c80bc13539a27b7053
 		super();
 		this.message_id = message_id;
 		this.content = content;
