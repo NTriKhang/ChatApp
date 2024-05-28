bd8a4340fad2235a784bc2624755970e836da319
ThanhLuan <ntluan51@gmail.com>
2024-05-28 21:20:57 +0700

    a

--- chat-app-client/src/App.jsx
+++ chat-app-client/src/App.jsx
@@ -4,6 +4,7 @@ import SetAvatar from "./components/SetAvatar";
 import ChatPage from "./pages/ChatPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./utils/ProtectedRoute";
 export default function App() {
   return (
     <BrowserRouter>
